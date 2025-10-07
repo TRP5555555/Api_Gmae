ecbc8d9718b7a52fec25675d5a19d6079821aa69
TRP5555555 <11xxcalibur@gmail.com>
2025-10-07 23:42:31 +0700

    test

--- app.ts
+++ app.ts
@@ -1,8 +1,8 @@
 // server.ts
 import express from "express";
-import cors from "cors";
 import session from "express-session";
 import path from "path";
+import cors from 'cors';
 
 import { router as users } from "./controller/index";
 import { router as upload } from "./controller/upload";
@@ -10,7 +10,15 @@ import { router as upload } from "./controller/upload";
 
 export const app = express();
 
-app.use(cors({ origin: "http://localhost:4200", credentials: true }));
+
+app.use(cors({
+    origin: 'http://localhost:4200',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+//app.use(cors({ origin: "http://localhost:4200", credentials: true }));
 app.use(express.json({ limit: "5mb" }));           
 export const UPLOAD_DIR = path.resolve(process.cwd(), 'uploads');
 
