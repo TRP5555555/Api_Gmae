8b7b1979265cf6a612831e99c176963fb15aa2aa
TRP5555555 <11xxcalibur@gmail.com>
2025-10-09 01:04:01 +0700

    test22

--- app.ts
+++ app.ts
@@ -10,7 +10,15 @@ import { router as upload } from "./controller/upload";
 
 export const app = express();
 
-app.use(cors({ origin: "http://localhost:4200", credentials: true }));
+
+app.use(cors({
+    origin: 'http://localhost:4200',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+//app.use(cors({ origin: "http://localhost:4200", credentials: true }));
 app.use(express.json({ limit: "5mb" }));           
 export const UPLOAD_DIR = path.resolve(process.cwd(), 'uploads');
 
